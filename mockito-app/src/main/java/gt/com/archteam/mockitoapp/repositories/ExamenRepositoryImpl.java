45712631f9e688f225965b9eae4cd4465ba7a092
Marvin Lopez <lopez.marvin9@gmail.com>
2024-07-07 14:32:02 -0600

    first tests with mockito

--- mockito-app/src/main/java/gt/com/archteam/mockitoapp/repositories/ExamenRepositoryImpl.java
+++ mockito-app/src/main/java/gt/com/archteam/mockitoapp/repositories/ExamenRepositoryImpl.java
@@ -1,6 +1,6 @@
 package gt.com.archteam.mockitoapp.repositories;
 
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import gt.com.archteam.mockitoapp.models.Examen;
@@ -9,7 +9,8 @@ public class ExamenRepositoryImpl implements ExamenRepository {
 
     @Override
     public List<Examen> findAll() {
-        return Arrays.asList(new Examen(5L, "Matematicas"), new Examen(6L, "Lenguaje"), new Examen(7L, "Historia"));
+        return Collections.emptyList();
+        // return Arrays.asList(new Examen(5L, "Matematicas"), new Examen(6L, "Lenguaje"), new Examen(7L, "Historia"));
     }
 
 }
--- mockito-app/src/test/java/gt/com/archteam/mockitoapp/services/ExamenServiceImplTest.java
+++ mockito-app/src/test/java/gt/com/archteam/mockitoapp/services/ExamenServiceImplTest.java
@@ -3,20 +3,42 @@ package gt.com.archteam.mockitoapp.services;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.*;
 
 import gt.com.archteam.mockitoapp.models.Examen;
 import gt.com.archteam.mockitoapp.repositories.ExamenRepository;
-import gt.com.archteam.mockitoapp.repositories.ExamenRepositoryImpl;
 
 class ExamenServiceImplTest {
 
     @Test
     void testFindExamenPorNombre() {
-        ExamenRepository repository = new ExamenRepositoryImpl();
+        ExamenRepository repository = mock(ExamenRepository.class);
         ExamenService service = new ExamenServiceImpl(repository);
+        var datos = Arrays.asList(new Examen(5L, "Matematicas"), new Examen(6L, "Lenguaje"),
+                new Examen(7L, "Historia"));
+
+        when(repository.findAll()).thenReturn(datos);
         Examen examen = service.findExamenPorNombre("Matematicas");
-        
+
+        assertNotNull(examen);
+        assertEquals(5L, examen.getId());
+        assertEquals("Matematicas", examen.getNombre());
+    }
+
+    @Test
+    void testFindExamenPorNombreListaVacia() {
+        ExamenRepository repository = mock(ExamenRepository.class);
+        ExamenService service = new ExamenServiceImpl(repository);
+        List<Examen> datos = Collections.emptyList();
+
+        when(repository.findAll()).thenReturn(datos);
+        Examen examen = service.findExamenPorNombre("Matematicas");
+
         assertNotNull(examen);
         assertEquals(5L, examen.getId());
         assertEquals("Matematicas", examen.getNombre());
